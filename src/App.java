6ea229cfd5ab1950ce9402801845021ce7a014e4
Manana-19 <Mananjerath9582@gmail.com>
2024-12-17 23:07:48 +0530

    Initial Commit with no plans at the moment

---
+++ src/App.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+
+
+class smth extends JFrame implements ActionListener{
+
+    private JLabel Result;
+    private int CurrentNum;
+    smth() {
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
+
