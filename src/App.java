8d324459fee4e68da1a287869f99a8a338979362
Manana-19 <Mananjerath9582@gmail.com>
2024-12-19 22:22:48 +0530

    Just a little progress.. (Can't get enough time daily)

--- src/App.java
+++ src/App.java
@@ -6,19 +6,53 @@ import java.awt.event.ActionListener;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 
 
 class smth extends JFrame implements ActionListener{
 
     private JLabel Result;
-    private int CurrentNum;
+    private double CurrentNum;
+
+    private JLabel MainFrame;
     smth() {
         
+        // Initializing the basic frame
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(new Dimension(400,900));
+        this.getContentPane().setBackground(Color.GRAY);
+        this.setLayout(new BorderLayout());
+
+
+        // Initializing basic Buttons for the Calculator to make it functional
+        JLabel Calc_Display = new JLabel();
+        this.setLayout(new GridLayout());
+
+
+
+        // Buttons for expressions
+        JLabel[] Req_Buttons = new JLabel[16];
+
+
+
+        // Button for clear
+
+
+        this.add(Calc_Display, BorderLayout.CENTER);
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+}
+
+public class App {
+    public static void main(String[] args) {
+        new smth();
     }
 }
 
