e6bd6d6e4871c0da2587d3b6b8883dd9fcc4c2c1
Manana-19 <Mananjerath9582@gmail.com>
2024-12-27 21:05:12 +0530

    Some Progress

--- src/App.java
+++ src/App.java
@@ -19,20 +19,27 @@ class smth extends JFrame implements ActionListener{
         // Initializing the basic frame
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setPreferredSize(new Dimension(700,700));
-        this.setSize(600, 800);
-        // this.getContentPane().setBackground(Color.GRAY);
+        this.setPreferredSize(new Dimension(600,800));
+        this.setSize(600, 900);
+        this.setResizable(true);
+        this.setMinimumSize(new Dimension(400,400));
+        this.getContentPane().setBackground(Color.GRAY);
         this.setLayout(new BorderLayout());
-
+        int wide = this.getWidth();
 
         // Initializing basic labels for the Calculator to make it functional
 
         Result = new JLabel();
 
-        Result.setText("Calculations Here");
-        Result.setPreferredSize(new Dimension(200,700));
+        Result.setText("Calc Here");
+        Result.setSize(new Dimension(wide,this.getHeight()/5));
+        Result.setPreferredSize(new Dimension(wide,this.getHeight()/5));
+        Result.setMinimumSize(new Dimension(wide,50));
         Result.setForeground(Color.WHITE);
         Result.setBackground(Color.BLACK);
+        Result.setOpaque(true);
+        Result.setVerticalTextPosition(JLabel.TOP);
+        Result.setHorizontalAlignment(JLabel.RIGHT);
 
 
 
@@ -42,40 +49,43 @@ class smth extends JFrame implements ActionListener{
 
         Calc_layout.setColumns(4);
         Calc_layout.setRows(4);
-        Calc_layout.setHgap(0);
-        Calc_layout.setVgap(1);
+        Calc_layout.setHgap(1);
+        Calc_layout.setVgap(3);
+        Calc_layout.preferredLayoutSize(Calc_Display);
 
-        Calc_Display.setLayout(Calc_layout);
-
+        // Setting the layout for the Buttons
 
         Calc_Display.setVisible(true);
-        Calc_Display.setPreferredSize(new Dimension(500,400));
-
+        Calc_Display.setSize(new Dimension(wide,4*(this.getHeight()/5)));
+        Calc_Display.setPreferredSize(new Dimension(wide,4*(this.getHeight()/5)));
+        Calc_Display.setMinimumSize(new Dimension(wide,200));
 
 
         // Buttons for expressions
-        String[] BtnNames = {"1","2","3","4","5","6","7","8","9","0","+","-","X","÷","=","Clear"};
+        String[] BtnNames = {"1","2","3","4","5","6","7","8","9","0","+","-","X","÷","=","C"};
 
         for (int x = 0; x<16; x++) {
             JButton thisBtn = new JButton();
-            thisBtn.setName(BtnNames[x]);
+            thisBtn.setText(BtnNames[x]);
             thisBtn.setVisible(true);
-            thisBtn.setPreferredSize(new Dimension(35,40));
+            thisBtn.setSize(new Dimension(wide/4,5));
+            thisBtn.setPreferredSize(new Dimension(wide/4,5));
+            thisBtn.setMinimumSize(new Dimension(wide/4,5));
             thisBtn.setVerticalTextPosition(JButton.CENTER);
             thisBtn.setHorizontalTextPosition(JButton.CENTER);
+            thisBtn.setBackground(Color.WHITE);
+            thisBtn.setForeground(Color.BLACK);
+            thisBtn.setOpaque(true);
 
-            System.out.println(thisBtn);
             Calc_Display.add(thisBtn);
         }
 
-
+        Calc_Display.setLayout(Calc_layout);
 
         // Button for clear
 
         this.add(Result, BorderLayout.NORTH);
         this.add(Calc_Display, BorderLayout.SOUTH);
-
-        this.pack();
 
     }
 
